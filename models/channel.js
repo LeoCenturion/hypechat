726cb6224bbbc38804d6117114c2c1d0c19876ff
flortincab <flortincab@gmail.com>
2019-06-07 14:22:04 -0300

    nombre de canales y organizaciones son unicos y requeridos

--- models/channel.js
+++ models/channel.js
@@ -7,7 +7,7 @@ const Schema = mongoose.Schema;
 const ChannelSchema = new Schema({
 	private: {type: Boolean, required: true, default: false},//siempre es publico si no se indica lo contrario
 	id: {type:String, required: true},
-	name: {type:String, required: true, default:''},
+	name: {type:String, required: true, unique:true},
 	owner: {type:String, required: true},
 	members: {type: Array, default: []},
 	description: {type: String, default: ''},

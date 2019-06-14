4f750cfe97f0d63605bcff7d86510297af204ca5
flortincab <flortincab@gmail.com>
2019-06-14 20:50:14 -0300

    pongo los test en el mismo archivo para ver si el coveralls lo toma bien

--- test/test.js
+++ test/test.js
@@ -9,6 +9,9 @@ let should = chai.should();
 
 const userControllers = require('../controllers/user');
 const User = require('../models/user');
+const organizationControllers = require('../controllers/organization');
+const Organization = require('../models/organization');
+const PrivateMsj = require('../models/privateMsj');
 
 userMock = {
 			_id: '1234qwer',
@@ -216,4 +219,70 @@ describe('USER', () => {
 		done();
    	});
 
+});
+
+
+describe('ORGANIZATION', () => {
+    let mongoStub = null;
+    let findOneUserStub = null; 
+    let findOrganizationStub = null;
+    let findPrivateMsjStub = null;
+    let findOneOrganizationStub = null;
+
+    beforeEach(() => {
+        mongoStub = sinon.stub(mongoose, 'connect').callsFake(() => {});
+        findOneUserStub = sinon.stub(User, 'findOne').callsFake((_, cb)=> cb(null, userMock));
+        findOrganizationStub = sinon.stub(Organization, 'find').callsFake((_, cb)=> cb(null, userMock.organizations));
+    	findPrivateMsjStub = sinon.stub(PrivateMsj, 'find').callsFake((_, cb)=> cb(null, [{email_user2:'msj_usr2', email_user1:'msj_usr1'}]));
+    	findOneOrganizationStub = sinon.stub(Organization, 'findOne').callsFake((_, cb)=> cb(null, null));
+    });
+
+    afterEach(() => {
+        mongoStub.restore();
+        findOneUserStub.restore();
+        findOrganizationStub.restore();
+        findPrivateMsjStub.restore();
+        findOneOrganizationStub.restore();
+    });
+
+    it('getUserOrganizations succesfull', (done) => {
+
+        req = {params:{email:userMock.email}}
+		res = {status: function(nro){assert.equal(nro,200)
+			return {send:function(obj){
+							obj.should.have.property('organizations');
+							//obj.organizations.should.be.equal(userMock.organizations);
+							return obj}}}}
+		
+		organizationControllers.getUserOrganizations(req,res)
+		done();
+   	});
+
+   	it('getPrivateMsj succesfull', (done) => {
+        req = {body:{token:'userMockToken',
+    				id: 'idOrganization',
+    				email: userMock.email}}
+		res = {status: function(nro){assert.equal(nro,200)
+			return {send:function(obj){
+							obj.should.have.property('msjs');
+							//obj.msjs.should.be.equal( [ 'msj_usr2', 'msj_usr1' ]);
+							return obj}}}}
+		
+		organizationControllers.getPrivateMsj(req,res)
+		done();
+   	});
+
+   	it('isOrganizationIDValid succesfull', (done) => {
+        req = {params:{organizationID: 'idOrganization'}}
+		res = {status: function(nro){assert.equal(nro,200)
+			return {send:function(obj){
+							obj.should.have.property('message');
+							return obj}}}}
+		
+		organizationControllers.isOrganizationIDValid(req,res)
+		done();
+   	});
+   	
+
+   	
 });

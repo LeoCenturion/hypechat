99a72e2b8f99cd0dcc22c2bbbf3819906a28c8b4
flortincab <flortincab@gmail.com>
2019-06-23 21:03:43 -0300

    cambio en un nombre

--- controllers/organization.js
+++ controllers/organization.js
@@ -509,7 +509,7 @@ function checkMessage(req, res){
 			for(let i=0; i<restrictedWords.length; i++){
 				message = message.split(restrictedWords[i]).join('***')
 			}
-			Channel.findOne({id:req.body.channelID},(err,channel)=>{
+			Channel.findOne({_id:req.body.channelID},(err,channel)=>{
 				if(err) return res.status(500).send({message: `Error al buscar un canal: ${err}`})
 				if(channel){
 					//sumo un mensaje al channel si existe, es decir que no es null
